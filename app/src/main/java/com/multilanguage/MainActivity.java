b9e61b3a0ad2d21643f62bbe081c2dd0fd172cd6
a2012s <wjj99@qq.com>
2020-07-30 15:01:05 +0800

    转为支持androidx

--- app/src/main/java/com/multilanguage/MainActivity.java
+++ app/src/main/java/com/multilanguage/MainActivity.java
@@ -1,11 +1,14 @@
 package com.multilanguage;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.multilanguage.utils.MultiLanguageUtil;
 import com.multilanguage.utils.SpUtil;
 import java.util.Locale;
